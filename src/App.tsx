22aaab397fc7cf2b5009e0552abae575a0e02f34
Michał Kardyś <michal@kardys.dev>
2021-08-19 15:27:38 +0200

    feat: scaffold autoupdater

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import semver from 'semver';
 
 type SemVer = string & { readonly type: unique symbol }
 type ComponentName = string & { readonly type: unique symbol }
@@ -7,14 +8,41 @@ type ComponentName = string & { readonly type: unique symbol }
 type Component = {
   name: ComponentName
   version: SemVer
+  range: SemVer
 }
 
-function localVersions(name: ComponentName) {
+type ComponentPackage = unknown;
 
+function loadedVersions(name: ComponentName): SemVer[] {
+
+  return [];
+}
+
+function maxSatysfying(versions: SemVer[], range: SemVer){
+  return semver.maxSatysfying(versions, range)
+}
+
+function findCompatible(component: Component) {
+  const versions = loadedVersions(component.name);
+  return maxSatysfying(versions, component.range);
+}
+
+function loadFromMemory(component: Component): ComponentPackage {
+  return '';
+}
+
+function loadExternal(component: Component): ComponentPackage {
+  return ''
 }
 
 function loadComponent(component: Component){
+  const compatibleVersion = findCompatible(component)
 
+  if (compatibleVersion){
+    return loadFromMemory({ ...component, version: compatibleVersion})
+  }
+
+  return loadExternal(component)
 }
 
 const App = () => {
