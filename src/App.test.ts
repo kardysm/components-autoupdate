750c57c63ad7349e0977c3170e6df503e6883814
Michał Kardyś <michal@kardys.dev>
2021-08-27 13:26:00 +0200

    wip: tests

--- src/App.test.ts
+++ src/App.test.ts
@@ -1,7 +1,5 @@
-import App, { RequireComponent } from "./Component.types"
 
-describe("App", () => {
-  it("should work", function () {
-    App()({ name: "no", version: "1.0.0" } as unknown as RequireComponent)
+describe.skip("App", () => {
+  it.skip("should work", () => {
   })
 })
